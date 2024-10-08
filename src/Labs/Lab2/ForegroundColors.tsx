efc0108c9aac78e500b50b61868b2ddc5ced2857
eriks-son <antgus03@gmail.com>
2024-10-08 15:32:51 -0400

    Start A2

---
+++ src/Labs/Lab2/ForegroundColors.tsx
@@ -0,0 +1,13 @@
+export default function ForegroundColors() {
+  return (
+<div id="wd-css-background-colors">
+  <h3 className="wd-fg-color-blue">Foreground color</h3>
+  <p className="wd-fg-color-red">
+    This text in this paragraph is red but 
+    <span className="wd-fg-color-green">
+      this text is green
+    </span>
+  </p>
+</div>
+  )
+}
